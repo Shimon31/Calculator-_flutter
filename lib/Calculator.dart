6f7a3e2e0dccac6f5ff9dc38a6a189d22d0ad768
Shimon <shimonbhuiya02@gmail.com>
2025-09-18 19:35:57 +0600

    add some design

--- lib/Calculator.dart
+++ lib/Calculator.dart
@@ -1,6 +1,4 @@
-import 'dart:ffi';
 
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class Calculator extends StatefulWidget {
@@ -11,22 +9,22 @@ class Calculator extends StatefulWidget {
 }
 
 class CalculatorState extends State<Calculator> {
-  Map<String, double> formValues = {"num1": 0, "num2": 0, "sum": 0};
 
-  void num1Change(value) {
-    formValues.update("num1", (v) => double.parse(value));
+  Map<String,double> formValues = {"num1":0,"num2":0,"sum":0};
+
+  void num1Change(value){
+    formValues.update("num1",(v)=>double.parse(value));
   }
 
-  void num2Change(value) {
-    formValues.update("num2", (v) => double.parse(value));
+  void num2Change(value){
+    formValues.update("num2",(v)=>double.parse(value) );
   }
-
-  void add() {
-    double num1 = formValues["num1"] ?? 0.0;
-    double num2 = formValues["num2"] ?? 0.0;
-    double sum = num1 + num2;
+  void add(){
+    double num1 = formValues["num1"]??0.0;
+    double num2 = formValues["num2"]??0.0;
+    double sum = num1+num2;
     setState(() {
-      formValues.update("sum", (v) => sum);
+      formValues.update("sum",(v)=> sum );
     });
   }
 
@@ -35,29 +33,65 @@ class CalculatorState extends State<Calculator> {
     return Scaffold(
       appBar: AppBar(title: Text("Calculator")),
       body: Padding(
-        padding: const EdgeInsets.all(40),
+        padding: EdgeInsets.all(40),
         child: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              TextFormField(
-                onChanged: (value) {
-                  num1Change(value);
-                },
+              TextFormField(onChanged: (value){
+                num1Change(value);
+              },
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  labelText: "Enter first Number",
+                  hintText: "ex: 12",
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                ),
               ),
               SizedBox(height: 20),
-              TextFormField(
-                onChanged: (value) {
-                  num2Change(value);
-                },
+              TextFormField(onChanged: (value){
+                num2Change(value);
+              },
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  labelText: "Enter second Number",
+                  hintText: "ex: 10",
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                ),
               ),
               SizedBox(height: 20),
               ElevatedButton(
-                onPressed: () {add();},
-                child: Text("Add", style: TextStyle(color: Colors.green[900])),
+                onPressed: () {
+                  add();
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.red,
+                  foregroundColor: Colors.white,
+                  elevation: 10,
+                  padding: EdgeInsets.symmetric(horizontal: 40, vertical: 10),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                ),
+                child: Text(
+                  "Add",
+                  style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                ),
               ),
-              
-              Text("Result: ${formValues["sum"].toString()}")
+
+              SizedBox(height: 20),
+              Text(
+                "Result:${formValues["sum"]}",
+                style: TextStyle(
+                  color: Colors.green,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 30,
+                ),
+              ),
             ],
           ),
         ),
